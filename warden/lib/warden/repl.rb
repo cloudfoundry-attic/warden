de08b61292cd4afb1c54c1a270c33d9a7713c676
Kowshik Prakasam <kprakasam@rbcon.com>
2012-06-15 16:17:05 -0700

    Printing of exit status/stdout/stderr for 'link' command. Pretty printing for all other server responses.
    Change-Id: I62fabbabb606a747f933b12ccb325ee787fa55bf

--- warden/lib/warden/repl.rb
+++ warden/lib/warden/repl.rb
@@ -2,6 +2,7 @@ require "readline"
 require "shellwords"
 require "warden/client"
 require "json"
+require "pp"
 
 module Warden
   class Repl
@@ -149,9 +150,7 @@ EOT
       begin
         command_info[:result] = @client.read
         case words[0]
-        when 'create'
-          puts command_info[:result]
-        when 'run'
+        when 'run', 'link'
           status, stdout, stderr = command_info[:result]
           puts "exit status: #{status}"
           puts
@@ -162,7 +161,7 @@ EOT
           puts stderr
           puts
         else
-          puts command_info[:result].inspect
+          pp command_info[:result]
         end
       rescue  => e
         command_info[:error] = e
