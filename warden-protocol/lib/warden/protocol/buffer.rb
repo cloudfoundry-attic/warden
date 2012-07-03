faa371204322dcd8987c3feaab42823f30034539
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-07-03 09:59:10 -0700

    Add buffer class to warden-protocol
    Change-Id: I7ec70c31c25246cfd400d13297236e7ce4f3702e

---
+++ warden-protocol/lib/warden/protocol/buffer.rb
@@ -0,0 +1,67 @@
+require "warden/protocol/base"
+
+module Warden
+  module Protocol
+    class Buffer
+      CRLF = "\r\n"
+
+      def self.request_to_wire(request)
+        unless request.kind_of?(BaseRequest)
+          raise ArgumentError, "Expected #kind_of? ::%s" % BaseRequest.name
+        end
+        payload_to_wire request.wrap.encode.to_s
+      end
+
+      def self.response_to_wire(response)
+        unless response.kind_of?(BaseResponse)
+          raise ArgumentError, "Expected #kind_of? ::%s" % BaseResponse.name
+        end
+        payload_to_wire response.wrap.encode.to_s
+      end
+
+      def initialize
+        @buffer = ""
+      end
+
+      def <<(data)
+        @buffer += data
+      end
+
+      def each_request(&blk)
+        each do |payload|
+          yield(Warden::Protocol::WrappedRequest.decode(payload).request)
+        end
+      end
+
+      def each_response(&blk)
+        each do |payload|
+          yield(Warden::Protocol::WrappedResponse.decode(payload).response)
+        end
+      end
+
+      protected
+
+      def self.payload_to_wire(payload)
+        payload.to_s.length.to_s + CRLF + payload.to_s + CRLF
+      end
+
+      def each
+        loop do
+          crlf = @buffer.index(CRLF)
+          break unless crlf
+
+          length = Integer(@buffer[0...crlf])
+          protocol_length = crlf + 2 + length + 2
+          break unless @buffer.length >= protocol_length
+
+          payload = @buffer[crlf + 2, length]
+
+          # Trim buffer
+          @buffer = @buffer[protocol_length..-1]
+
+          yield(payload)
+        end
+      end
+    end
+  end
+end
---
+++ warden-protocol/spec/buffer_spec.rb
@@ -0,0 +1,64 @@
+require "spec_helper"
+require "warden/protocol/buffer"
+require "warden/protocol/echo"
+
+describe Warden::Protocol::Buffer do
+  let(:request) { Warden::Protocol::EchoRequest.new(:message => "request") }
+  let(:response) { Warden::Protocol::EchoResponse.new(:message => "response") }
+
+  it "should support iterating over requests" do
+    subject << Warden::Protocol::Buffer.request_to_wire(request)
+    subject.each_request do |request|
+      request.class.should == Warden::Protocol::EchoRequest
+      request.message.should == "request"
+    end
+  end
+
+  it "should support iterating over responses" do
+    subject << Warden::Protocol::Buffer.response_to_wire(response)
+    subject.each_response do |response|
+      response.class.should == Warden::Protocol::EchoResponse
+      response.message.should == "response"
+    end
+  end
+
+  describe "fuzzing" do
+    it "should not break request iteration" do
+      data = Warden::Protocol::Buffer.request_to_wire(request)
+
+      loop do
+        chunk = data.slice!(0)
+        subject << chunk
+        break if data.empty?
+
+        subject.each_request do
+          fail
+        end
+      end
+
+      subject.each_request do |request|
+        request.class.should == Warden::Protocol::EchoRequest
+        request.message.should == "request"
+      end
+    end
+
+    it "should not break response iteration" do
+      data = Warden::Protocol::Buffer.response_to_wire(response)
+
+      loop do
+        chunk = data.slice!(0)
+        subject << chunk
+        break if data.empty?
+
+        subject.each_response do
+          fail
+        end
+      end
+
+      subject.each_response do |response|
+        response.class.should == Warden::Protocol::EchoResponse
+        response.message.should == "response"
+      end
+    end
+  end
+end
