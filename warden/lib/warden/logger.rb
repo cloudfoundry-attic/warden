c921c19e87a9936f3b952726da48349ea616c98d
Kowshik Prakasam <kprakasam@rbcon.com>
2012-07-25 19:24:34 -0700

    Use Steno in Warden.
    Change-Id: Ie3ce94f071431c0e6c919b95ca0ea27b7d3a0324

--- warden/lib/warden/logger.rb
+++ warden/lib/warden/logger.rb
@@ -1,17 +1,18 @@
 # coding: UTF-8
 
-require "vcap/logging"
+require "steno"
 
 module Warden
 
   module Logger
 
     def self.setup_logger(config = {})
-      VCAP::Logging.reset
-      VCAP::Logging.setup_from_config(config)
+      steno_config = Steno::Config.to_config_hash(config)
+      steno_config[:context] = Steno::Context::FiberLocal.new
+      Steno.init(Steno::Config.new(steno_config))
 
       # Override existing logger instance
-      @logger = VCAP::Logging.logger("warden")
+      @logger = Steno.logger("warden")
     end
 
     def self.logger?
@@ -19,46 +20,20 @@ module Warden
     end
 
     def self.logger
-      @logger ||= setup_logger(:level => :info)
+      @logger ||= setup_logger("level" => "info")
     end
 
     def self.logger=(logger)
       @logger = logger
     end
 
-    VCAP::Logging::LOG_LEVELS.each_key do |level|
+
+    Steno::Logger::LEVELS.each_key do |level|
       define_method(level) do |*args|
-        prefix = logger_prefix_from_stack caller(1).first
-        fmt = args.shift
-        fmt = "%s: %s" % [prefix, fmt] if prefix
-        Logger.logger.send(level, fmt, *args) if Logger.logger?
+        message = args.shift
+        message = message.to_s unless message.is_a? String
+        Logger.logger.send(level, message) if Logger.logger? and message
       end
-    end
-
-    protected
-
-    def logger_prefix_from_stack(str)
-      m = str.match(/^(.*):(\d+):in `(.*)'$/i)
-      file, line, method = m[1], m[2], m[3]
-
-      file_parts = File.expand_path(file).split("/")
-      trimmed_file = file_parts.
-        reverse.
-        take_while { |e| e != "lib" }.
-        map.
-        with_index { |e,i| i == 0 ? e : e[0, 1] }.
-        reverse.
-        join("/")
-
-      class_parts = self.class.name.split("::")
-      trimmed_class = class_parts.
-        reverse.
-        map.
-        with_index { |e,i| i == 0 ? e : e[0, 1].upcase }.
-        reverse.
-        join("::")
-
-      "%s:%s - %s#%s" % [trimmed_file, line, trimmed_class, method]
     end
   end
 end
