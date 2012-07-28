d02b12396bc7f5f6841751fdc39afbead7f3a9c7
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-07-27 18:37:40 -0700

    Remove container's cgroups on destroy
    Change-Id: Ie41b0609e38bdad69743c289d81428a3c2dfe5a1

--- warden/root/linux/skeleton/destroy.sh
+++ warden/root/linux/skeleton/destroy.sh
@@ -7,6 +7,8 @@ shopt -s nullglob
 
 cd $(dirname "${0}")
 
+source ./config
+
 ./net.sh teardown
 
 if [ -f ppid ]
@@ -14,12 +16,34 @@ then
   ppid=$(cat ppid)
   rm -f ppid
 
+  path=/sys/fs/cgroup/cpu/instance-$id
+  tasks=$path/tasks
+
   while true
   do
     kill -9 $ppid 2> /dev/null || true
-    [ ! -d /proc/$ppid ] && exit 0
-    sleep 0.1
+
+    # Wait while there are tasks in one of the instance's cgroups
+    if [ -f $tasks ] && [ -n "$(cat $tasks)" ]
+    then
+      sleep 0.1
+    else
+      break
+    fi
   done
+
+  # Remove cgroups
+  for system_path in /sys/fs/cgroup/*
+  do
+    path=/sys/fs/cgroup/cpu/instance-$id
+
+    if [ -d $path ]
+    then
+      rmdir $path
+    fi
+  done
+
+  exit 0
 fi
 
 exit 1
