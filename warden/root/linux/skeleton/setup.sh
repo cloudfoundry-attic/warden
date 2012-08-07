afe9dbefc21e1a355f3f1cbd8f1d0ded0522899e
mpage <mpage@rbcon.com>
2012-08-07 11:19:29 -0700

    Shorten control file path for persistent ssh connections
    These are unix domain sockets and, as such, are limited to the length allowed
    by sockaddr_un.
    
    Change-Id: I86e7be15e3455925ac467e4330d9046c05009bf9

--- warden/root/linux/skeleton/setup.sh
+++ warden/root/linux/skeleton/setup.sh
@@ -130,7 +130,7 @@ cat <<-EOS > ssh/ssh_config
 StrictHostKeyChecking yes
 UserKnownHostsFile $(pwd)/ssh/known_hosts
 IdentityFile $(pwd)/ssh/access_key
-ControlPath $(pwd)/ssh/control-%r@%h
+ControlPath $(pwd)/ssh/ctl-%r@%h
 Host container
 HostName ${network_container_ip}
 Host ${id}
