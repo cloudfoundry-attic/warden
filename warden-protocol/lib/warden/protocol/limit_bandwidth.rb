1761300651b8a34b54046a6a1597fe81970cf871
andyzh <andyzhang@rbcon.com>
2012-08-15 06:13:20 -0600

    jkimplement bandwidth quota
    Change-Id: I6d84c677e08aab1ece474e956598b67731ab3b93

---
+++ warden-protocol/lib/warden/protocol/limit_bandwidth.rb
@@ -0,0 +1,18 @@
+# coding: UTF-8
+
+require "warden/protocol/base"
+
+module Warden
+  module Protocol
+    class LimitBandwidthRequest < BaseRequest
+      required :handle, :string, 1
+      required :rate, :uint64, 2
+      required :burst, :uint64, 3 
+    end
+
+    class LimitBandwidthResponse < BaseResponse
+      required :rate, :uint64, 1 
+      required :burst, :uint64, 2 
+    end
+  end
+end
