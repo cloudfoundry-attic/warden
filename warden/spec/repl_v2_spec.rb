3c6f11edc456933064e5a1da73be48850542d67e
Kowshik Prakasam <kprakasam@rbcon.com>
2012-08-17 04:55:16 -0700

    Repl v2.
    Change-Id: Id6bbd0be0f3564686b9008262ecc9cb9dadc01d4

---
+++ warden/spec/repl_v2_spec.rb
@@ -0,0 +1,51 @@
+# coding: UTF-8
+
+describe Warden::Repl do
+  describe "#new" do
+    it "should create a warden client" do
+    end
+  end
+
+  describe "#start" do
+    it "should connect to the server" do
+    end
+
+    it "should display the warden prompt" do
+    end
+
+    it "should read commands from stdin" do
+    end
+
+    it "should write error messages to stderr" do
+    end
+
+    it "should save the command history" do
+    end
+
+    it "should restore the command history" do
+    end
+
+    it "should perform keyword completion" do
+    end
+  end
+
+  describe "#process_command" do
+    it "should add command trace to output" do
+    end
+
+    it "should return global help" do
+    end
+
+    it "should return command help" do
+    end
+
+    it "should convert run command to spawn and stream commands" do
+    end
+
+    it "should serialize response from warden server" do
+    end
+
+    it "should wrap errors" do
+    end
+  end
+end
