bab6e06cac102bc05e9b3d5a6762f3b363b6e18a
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-09-24 17:42:03 -0700

    Update start/stop to use warden shell
    Change-Id: Id7a28ae45442ec3c78022d718e9bf69bebcbe8a1

--- warden/root/linux/skeleton/stop.sh
+++ warden/root/linux/skeleton/stop.sh
@@ -5,11 +5,13 @@ set -o nounset
 set -o errexit
 shopt -s nullglob
 
-cd $(dirname "${0}")
+cd $(dirname $0)
 
-if [ ! -f started ]; then
-  echo "Container is not running..."
+if [ ! -f ./run/wshd.pid ]
+then
+  echo "wshd is not running..."
   exit 1
 fi
 
-ssh -F ssh/ssh_config root@container /sbin/warden-stop.sh "$@"
+# TODO: make graceful
+kill -9 $(cat ./run/wshd.pid)
