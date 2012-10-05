241e9126f722ab9a9a67e057d64e4dc13c2f8924
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-10-05 01:04:37 -0700

    Forward resource limits to wshd
    With this change wshc reads resource limits from the environment and
    passes them to wshd, that applies them before executing a process.
    
    For every resource limit that is not specified, wshc falls back on
    a set of defaults. This is done to make sure that processes that are
    executed by wshd cannot inherit its resource limits.
    
    Change-Id: Ib8b6c194d1523c3f36b72bac158ce94aa9fca960

--- warden/src/wsh/msg.h
+++ warden/src/wsh/msg.h
@@ -4,7 +4,11 @@
 #define MSG_VERSION 1
 #define MSG_MAX_SIZE (16 * 1024)
 
+#include <sys/time.h>
+#include <sys/resource.h>
+
 typedef struct msg__array_s msg__array_t;
+typedef struct msg__rlimit_s msg__rlimit_t;
 typedef struct msg_request_s msg_request_t;
 typedef struct msg_response_s msg_response_t;
 
@@ -13,9 +17,18 @@ struct msg__array_s {
   char buf[8 * 1024];
 };
 
+struct msg__rlimit_s {
+  int count;
+  struct {
+    int id;
+    struct rlimit rlim;
+  } rlim[RLIMIT_NLIMITS];
+};
+
 struct msg_request_s {
   int version;
   int tty;
+  msg__rlimit_t rlim;
   msg__array_t arg;
 };
 
@@ -25,6 +38,9 @@ struct msg_response_s {
 
 int msg_array_import(msg__array_t * a, int count, const char ** ptr);
 const char ** msg_array_export(msg__array_t * a);
+
+int msg_rlimit_import(msg__rlimit_t *);
+int msg_rlimit_export(msg__rlimit_t *);
 
 void msg_request_init(msg_request_t *req);
 void msg_response_init(msg_response_t *res);
