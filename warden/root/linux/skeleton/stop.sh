763dca9a7aa6c087193afb85e36507041fa87d1b
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-10-09 10:37:31 -0700

    Make stop script graceful again
    Change-Id: Ia68fa48f19f8c359d136c82e3a95fd6972d9286c

--- warden/root/linux/skeleton/stop.sh
+++ warden/root/linux/skeleton/stop.sh
@@ -13,5 +13,53 @@ then
   exit 1
 fi
 
-# TODO: make graceful
-kill -9 $(cat ./run/wshd.pid)
+WAIT=10
+
+function usage() {
+  echo "Usage $0 [OPTION]..." >&2
+  echo "  -w N seconds to wait before sending SIGKILL;" >&2
+  echo "       N=0 skips SIGTERM and sends SIGKILL immediately" >&2
+  exit 1
+}
+
+while getopts ":w:h" opt
+do
+  case $opt in
+    "w")
+      WAIT=$OPTARG
+      ;;
+    "h")
+      usage
+      ;;
+    "?")
+      # Ignore invalid options
+      ;;
+  esac
+done
+
+function ms() {
+  echo $(($(date +%s%N)/1000000))
+}
+
+ms_start=$(ms)
+ms_end=$(($ms_start + ($WAIT * 1000)))
+
+# Send SIGTERM
+if [[ $(ms) -lt $ms_end ]]
+then
+  bin/wsh pkill -TERM -v -u 0 || true
+fi
+
+# Wait for processes to quit
+while [[ $(ms) -lt $ms_end ]]
+do
+  if ! bin/wsh pgrep -c -v -u 0 > /dev/null
+  then
+    exit 0
+  fi
+
+  sleep 1
+done
+
+# Send SIGKILL
+bin/wsh pkill -KILL -v -u 0 || true
