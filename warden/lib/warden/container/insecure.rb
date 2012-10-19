0d849f78aa2c124e05b5152ebf65e4d8dd1a6539
Kowshik Prakasam <kprakasam@rbcon.com>
2012-10-19 14:24:46 -0700

    Plumb resource limits from warden server to wsh.
    - Bump warden-protocol to latest version.
    
    Change-Id: Ie8524cfd03e1d2cc2c14d2a49f8c0d67785e2dc9

--- warden/lib/warden/container/insecure.rb
+++ warden/lib/warden/container/insecure.rb
@@ -41,7 +41,9 @@ module Warden
       end
 
       def create_job(request)
-        spawn_job(File.join(container_path, "run.sh"), :input => request.script)
+        spawn_job(File.join(container_path, "run.sh"),
+                  :input => request.script,
+                  :env => resource_limits(request))
       end
 
       def do_net_in(request, response)
