85574ccffbd2d2c23461a4b42bade14e6b8bac30
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-12-04 16:11:50 -0800

    Add config
    Change-Id: If043b460bded3f59576803ef5d4b6dd2693b2518

---
+++ go/src/warden/server/config/config.go
@@ -0,0 +1,124 @@
+package config
+
+import (
+	"io/ioutil"
+	"launchpad.net/goyaml"
+
+//	"warden/server/pool"
+)
+
+type quotaConfig struct {
+	DiskQuotaEnabled bool "disk_quota_enabled"
+}
+
+type rlimitsConfig struct {
+	As         int64 "as"
+	Core       int64 "core"
+	Cpu        int64 "cpu"
+	Data       int64 "data"
+	Fsize      int64 "fsize"
+	Locks      int64 "locks"
+	Memlock    int64 "memlock"
+	Msgqueue   int64 "msgqueue"
+	Nice       int64 "nice"
+	Nofile     int64 "nofile"
+	Nproc      int64 "nproc"
+	Rss        int64 "rss"
+	Rtprio     int64 "rtprio"
+	Sigpending int64 "sigpending"
+	Stack      int64 "stack"
+}
+
+type serverConfig struct {
+	UnixDomainPath        string "unix_domain_path"
+	UnixDomainPermissions int    "unix_domain_permissions"
+
+	ContainerRootfsPath string "container_rootfs_path"
+	ContainerDepotPath  string "container_depot_path"
+
+	ContainerGraceTime uint "container_grace_time"
+
+	ContainerRlimits rlimitsConfig "container_rlimits"
+
+	Quota quotaConfig "quota"
+}
+
+var defaultServerConfig = serverConfig{
+	UnixDomainPath:        "/tmp/warden.sock",
+	UnixDomainPermissions: 0755,
+
+	ContainerGraceTime: 300, // 5 minutes
+
+	ContainerRlimits: rlimitsConfig{
+		As:     4294967296,
+		Nofile: 8192,
+		Nproc:  512,
+	},
+
+	Quota: quotaConfig{DiskQuotaEnabled: true},
+}
+
+type networkConfig struct {
+	PoolStartAddress string "pool_start_address"
+	PoolSize         int    "pool_size"
+
+	DenyNetworks  []string "deny_networks"
+	AllowNetworks []string "allow_networks"
+}
+
+var defaultNetworkConfig = networkConfig{
+	PoolStartAddress: "10.254.0.0",
+	PoolSize:         256,
+}
+
+type userConfig struct {
+	PoolStartUid int "pool_start_uid"
+	PoolSize     int "pool_size"
+}
+
+var defaultUserConfig = userConfig{
+	PoolStartUid: 10000,
+	PoolSize:     256,
+}
+
+type Config struct {
+	Server  serverConfig  "server"
+	Network networkConfig "network"
+	User    userConfig    "user"
+}
+
+var defaultConfig = Config{
+	Server:  defaultServerConfig,
+	Network: defaultNetworkConfig,
+	User:    defaultUserConfig,
+}
+
+func init() {
+	defaultConfig.sanitize()
+}
+
+func (c *Config) sanitize() {
+	if c.Network.DenyNetworks == nil {
+		c.Network.DenyNetworks = make([]string, 0)
+	}
+	if c.Network.AllowNetworks == nil {
+		c.Network.AllowNetworks = make([]string, 0)
+	}
+}
+
+func InitConfigFromFile(path string) *Config {
+	var c Config = defaultConfig
+	var e error
+
+	b, e := ioutil.ReadFile(path)
+	if e != nil {
+		panic(e.Error())
+	}
+
+	e = goyaml.Unmarshal(b, &c)
+	if e != nil {
+		panic(e.Error())
+	}
+
+	return &c
+}
---
+++ go/src/warden/server/config/config_test.go
@@ -0,0 +1,183 @@
+package config
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/goyaml"
+)
+
+type ConfigSuite struct {
+	Config
+}
+
+var _ = Suite(&ConfigSuite{})
+
+func (s *ConfigSuite) SetUpTest(c *C) {
+	s.Config = defaultConfig
+}
+
+func (s *ConfigSuite) TestServerUnixDomain(c *C) {
+	var b = []byte(`
+server:
+  unix_domain_path: /tmp/whatever.sock
+  unix_domain_permissions: 0600
+`)
+
+	c.Check(s.Server.UnixDomainPath, Equals, "/tmp/warden.sock")
+	c.Check(s.Server.UnixDomainPermissions, Equals, 0755)
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Server.UnixDomainPath, Equals, "/tmp/whatever.sock")
+	c.Check(s.Server.UnixDomainPermissions, Equals, 0600)
+}
+
+func (s *ConfigSuite) TestServerContainerPath(c *C) {
+	var b = []byte(`
+server:
+  container_rootfs_path: /tmp/rootfs
+  container_depot_path: /tmp/depot
+`)
+
+	c.Check(s.Server.ContainerRootfsPath, Equals, "")
+	c.Check(s.Server.ContainerDepotPath, Equals, "")
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Server.ContainerRootfsPath, Equals, "/tmp/rootfs")
+	c.Check(s.Server.ContainerDepotPath, Equals, "/tmp/depot")
+}
+
+func (s *ConfigSuite) TestServerContainerGraceTime(c *C) {
+	var b = []byte(`
+server:
+  container_grace_time: 10
+`)
+
+	c.Check(s.Server.ContainerGraceTime, Equals, uint(300))
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Server.ContainerGraceTime, Equals, uint(10))
+}
+
+func (s *ConfigSuite) TestServerContainerRlimits(c *C) {
+	var b = []byte(`
+server:
+  container_rlimits:
+    as: 37
+    core: 37
+    cpu: 37
+    data: 37
+    fsize: 37
+    locks: 37
+    memlock: 37
+    msgqueue: 37
+    nice: 37
+    nofile: 37
+    nproc: 37
+    rss: 37
+    rtprio: 37
+    sigpending: 37
+    stack: 37
+`)
+
+	c.Check(s.Server.ContainerRlimits.As, Equals, int64(4294967296))
+	c.Check(s.Server.ContainerRlimits.Core, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Cpu, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Data, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Fsize, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Locks, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Memlock, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Msgqueue, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Nice, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Nofile, Equals, int64(8192))
+	c.Check(s.Server.ContainerRlimits.Nproc, Equals, int64(512))
+	c.Check(s.Server.ContainerRlimits.Rss, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Rtprio, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Sigpending, Equals, int64(0))
+	c.Check(s.Server.ContainerRlimits.Stack, Equals, int64(0))
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Server.ContainerRlimits.As, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Core, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Cpu, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Data, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Fsize, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Locks, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Memlock, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Msgqueue, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Nice, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Nofile, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Nproc, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Rss, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Rtprio, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Sigpending, Equals, int64(37))
+	c.Check(s.Server.ContainerRlimits.Stack, Equals, int64(37))
+}
+
+func (s *ConfigSuite) TestServerQuota(c *C) {
+	var b = []byte(`
+server:
+  quota:
+    disk_quota_enabled: false
+`)
+
+	c.Check(s.Server.Quota.DiskQuotaEnabled, Equals, true)
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Server.Quota.DiskQuotaEnabled, Equals, false)
+}
+
+func (s *ConfigSuite) TestNetworkPool(c *C) {
+	var b = []byte(`
+network:
+  pool_start_address: 10.0.0.0
+  pool_size: 1
+`)
+
+	c.Check(s.Network.PoolStartAddress, Equals, "10.254.0.0")
+	c.Check(s.Network.PoolSize, Equals, 256)
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Network.PoolStartAddress, Equals, "10.0.0.0")
+	c.Check(s.Network.PoolSize, Equals, 1)
+}
+
+func (s *ConfigSuite) TestNetworkDenyAllow(c *C) {
+	var b = []byte(`
+network:
+  deny_networks:
+    - 1.1.1.1
+    - 2.2.2.2
+  allow_networks:
+    - 3.3.3.3
+    - 4.4.4.4
+`)
+
+	c.Check(s.Network.DenyNetworks, DeepEquals, []string{})
+	c.Check(s.Network.AllowNetworks, DeepEquals, []string{})
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.Network.DenyNetworks, DeepEquals, []string{"1.1.1.1", "2.2.2.2"})
+	c.Check(s.Network.AllowNetworks, DeepEquals, []string{"3.3.3.3", "4.4.4.4"})
+}
+
+func (s *ConfigSuite) TestUserPool(c *C) {
+	var b = []byte(`
+user:
+  pool_start_uid: 37
+  pool_size: 1
+`)
+
+	c.Check(s.User.PoolStartUid, Equals, 10000)
+	c.Check(s.User.PoolSize, Equals, 256)
+
+	goyaml.Unmarshal(b, &s.Config)
+
+	c.Check(s.User.PoolStartUid, Equals, 37)
+	c.Check(s.User.PoolSize, Equals, 1)
+}
