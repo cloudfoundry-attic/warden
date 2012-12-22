3dfd7a7706ea4807303c2c71c43f144341dc5618
Tang Rui <rtang@vmware.com>
2012-12-21 22:27:55 -0700

    Fix the bug that warden shouldn't break nfs
    Change-Id: I2c4e5b5526f73011998c023812542e020b03930b

--- warden/root/linux/net.sh
+++ warden/root/linux/net.sh
@@ -127,6 +127,7 @@ function setup_nat() {
   # Enable NAT on outgoing traffic
   (iptables -t nat -S POSTROUTING | grep -q "\-j MASQUERADE\b") ||
     iptables -t nat -A POSTROUTING \
+      ! --out-interface "lo" \
       --jump MASQUERADE
 }
 
