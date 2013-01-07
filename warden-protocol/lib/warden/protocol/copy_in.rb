4b4b96d6f6ebd854cc85bdd810e6fae367747727
Anfernee Gui <agui@vmware.com>
2013-01-07 17:33:48 +0800

    Support copy_in by configurable owner
    Change-Id: Ic1c96542f5d99b4d4a180b755fdbd13e2760c0cb

--- warden-protocol/lib/warden/protocol/copy_in.rb
+++ warden-protocol/lib/warden/protocol/copy_in.rb
@@ -8,6 +8,7 @@ module Warden
       required :handle, :string, 1
       required :src_path, :string, 2
       required :dst_path, :string, 3
+      optional :owner, :string, 4
 
       def self.description
         "Copy files/directories into the container."
