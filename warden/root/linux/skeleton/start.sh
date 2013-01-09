94084030795c39d9280508caaf9b4a0b60c7af59
Pieter Noordhuis <pcnoordhuis@gmail.com>
2013-01-09 10:31:39 -0800

    Redirect stdout/stderr of wshd
    Change-Id: I96d915fe79250036e52b5b8dffae07954328d482

--- warden/root/linux/skeleton/start.sh
+++ warden/root/linux/skeleton/start.sh
@@ -17,4 +17,6 @@ fi
 
 ./net.sh setup
 
-./bin/wshd --run ./run --lib ./lib --root ./mnt --title "wshd: $id"
+./bin/wshd --run ./run --lib ./lib --root ./mnt --title "wshd: $id" \
+  1> ./run/wshd.out.log \
+  2> ./run/wshd.err.log
