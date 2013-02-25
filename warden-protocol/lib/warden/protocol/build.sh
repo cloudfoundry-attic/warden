81d45fd36fed86466bb3d7a9b08bbce40e926227
Pieter Noordhuis <pcnoordhuis@gmail.com>
2013-02-25 13:17:16 -0800

    Import native protocol buffer definitions

---
+++ warden-protocol/lib/warden/protocol/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd pb
+
+export BEEFCAKE_NAMESPACE=Warden::Protocol
+
+out=bundle
+(echo "package protocol;" && (find . -name '*.proto' | sort | xargs cat | sed /^package/d)) > $out
+protoc --beefcake_out=. $out
+sed -e "s/Beefcake::Message/Warden::Protocol::BaseMessage/" $out.pb.rb > ../pb.rb
+rm -f $out*
