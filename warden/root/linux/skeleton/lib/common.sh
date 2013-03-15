dc271e52955585082b00806c2c6e40cf1292216d
Dmitriy Kalinin and Pieter Noordhuis <pair+dmitriy+pieter@pivotallabs.com>
2013-03-14 17:17:07 -0700

    Setup file system using RO/RW bind mounts on non-Ubuntu systems

--- warden/root/linux/skeleton/lib/common.sh
+++ warden/root/linux/skeleton/lib/common.sh
@@ -17,7 +17,40 @@ function get_distrib_codename() {
   fi
 }
 
-function setup_fs() {
+function overlay_directory_in_rootfs() {
+  # Skip if exists
+  if [ ! -d tmp/rootfs/$1 ]
+  then
+    if [ -d mnt/$1 ]
+    then
+      cp -r mnt/$1 tmp/rootfs/
+    else
+      mkdir -p tmp/rootfs/$1
+    fi
+  fi
+
+  mount -n --bind tmp/rootfs/$1 mnt/$1
+  mount -n --bind -o remount,$2 tmp/rootfs/$1 mnt/$1
+}
+
+function setup_fs_other() {
+  mkdir -p tmp/rootfs mnt
+  mkdir -p $rootfs_path/proc
+
+  mount -n --bind $rootfs_path mnt
+  mount -n --bind -o remount,ro $rootfs_path mnt
+
+  overlay_directory_in_rootfs /dev rw
+  overlay_directory_in_rootfs /etc rw
+  overlay_directory_in_rootfs /home rw
+  overlay_directory_in_rootfs /sbin rw
+
+  mkdir -p tmp/rootfs/tmp
+  chmod 777 tmp/rootfs/tmp
+  overlay_directory_in_rootfs /tmp rw
+}
+
+function setup_fs_ubuntu() {
   mkdir -p tmp/rootfs mnt
 
   distrib_codename=$(get_distrib_codename)
@@ -34,6 +67,15 @@ function setup_fs() {
     exit 1
     ;;
   esac
+}
+
+function setup_fs() {
+  if grep -q -i ubuntu /etc/issue
+  then
+    setup_fs_ubuntu
+  else
+    setup_fs_other
+  fi
 }
 
 function teardown_fs() {
