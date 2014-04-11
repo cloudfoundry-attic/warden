4e438928913a97123dfa52a4bb741d3b5e59668f
Matthew Sykes <matthew.sykes@gmail.com>
2014-04-11 15:09:58 -0700

    Don't rsync .vagrant into the VM
    The rsync script does not exclude .vagrant when populating the test VM
    with the code so if you happened to `vagrant up` the warden test VM from
    the warden directory, you get your virtual machine copied into your
    other virtual machine.
    
    This simply excludes .vagrant from the sync.

--- bin/rsync_workspace_into_vm.sh
+++ bin/rsync_workspace_into_vm.sh
@@ -2,4 +2,4 @@
 set -e -x -u
 
 vagrant ssh-config > ssh_config
-rsync -arq --rsh="ssh -F ssh_config" $1/ default:workspace
+rsync -arq --rsh="ssh -F ssh_config" --exclude .vagrant $1/ default:workspace
