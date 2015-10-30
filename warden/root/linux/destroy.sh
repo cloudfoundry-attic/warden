f23104698beb7e5f7eb2c3d0c1f4d4fd9543c52a
Matthew Sykes <matthew.sykes@gmail.com>
2015-10-30 13:55:56 -0400

    Revert debug code

--- warden/root/linux/destroy.sh
+++ warden/root/linux/destroy.sh
@@ -23,15 +23,12 @@ if [ -d $target ]
 then
   if [ -f $target/destroy.sh ]
   then
-    echo "Calling container destroy"
     $target/destroy.sh
   fi
 
   # Retry 5 times to avoid ocational device busy
   count=0
-  echo "Attempting to dstroy container"
   until `rm -rf $target` || [ $count -eq 4 ]; do
-     echo "Attempt $count to destroy container"
      ((count++))
      sleep 0.3
   done
